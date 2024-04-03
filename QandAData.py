3438d90b70419ae3fbd4064968bf9e64a00c6943
Kimberlee Finch <kmfinch42@tntech.edu>
2024-04-02 22:48:16 -0500

    first try at adding a list as the a colum vale for a table

--- QandAData.py
+++ QandAData.py
@@ -5,3 +5,21 @@ import sqlite3
 
 conn= sqlite3.connect('QuarterA3.db')
 curse = conn.cursor()
+
+#Code to add another question
+#curse.execute('''INSERT INTO tableName VALUES
+#              ('Question?','Answer Options','Correct Answer)
+#              ''')
+
+
+#Table 1 labled ACCT
+curse.execute('''CREATE TABLE IF NOT EXISTS ACCT
+              (question TEXT, answerOptions TEXT, correctAnswer TEXT)''')
+
+AnswerList = ['Assets','Accounts Recivale', 'Owners Equity', 'Assistant']
+AnserOpt = str(AnswerList)
+curse.execute('''INSERT INTO ACCT VALUES
+              ('What does Liabilty + Equity=', AnserOpt, 'Assets')
+              ''')
+conn.commit()
+
