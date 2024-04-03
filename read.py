e8999487b54e3ff374519d9395390e04abbc1a86
Kimberlee Finch <kmfinch42@tntech.edu>
2024-04-03 09:32:07 -0500

    added all tables

--- read.py
+++ read.py
@@ -11,3 +11,19 @@ print()
 curse.execute("SELECT * FROM ACCT")
 print(curse.fetchall())
 print()
+
+curse.execute("SELECT * FROM Database")
+print(curse.fetchall())
+print()
+
+curse.execute("SELECT * FROM AssablyProg")
+print(curse.fetchall())
+print()
+
+curse.execute("SELECT * FROM PythonProg")
+print(curse.fetchall())
+print()
+
+curse.execute("SELECT * FROM ComputerHardWear")
+print(curse.fetchall())
+print()
