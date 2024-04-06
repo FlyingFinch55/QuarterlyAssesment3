031dde50b1f592118f7637588c69659241acbb43
Kimberlee Finch <kmfinch42@tntech.edu>
2024-04-05 22:37:11 -0500

    finished all 10 questions for table Database

--- QandAData.py
+++ QandAData.py
@@ -35,24 +35,29 @@ curse.execute('''CREATE TABLE IF NOT EXISTS Database
               (question TEXT, answerOption1 TEXT, answerOption2 TEXT, answerOption3 TEXT, answerOption4 TEXT, correctAnswer TEXT)''')
 
 
-curse.execute('''INSERT INTO Database VALUES
-              ('Relationships are made using what?','Complemt Key','Foreign Key','Emotions','Two entitys','Foreign Key')
-              ''')
-conn.commit()
+
 
 #1('Name the key that is the Primary Key of another entity.','Foreign Key','Compound Key','Piano Key','Skeleton Key', 'Foreign Key')
 #2('Name the key that is made of more than one attribute.', 'Compound Key','Foreign Key','Pinao Key','Skeleton Key','Compound Key')
 #3('True or False, the Primary Key is not a unique attribute.','True','False','','','False' )
 #4('True or False, there are 3 main types of anomalies.','True','False','','','True')
 #5('Relationships are made using what?','Complemt Key','Foreign Key','Emotions','Two entitys','Foreign Key')
-
+#6('What color on the stop light method are 1 to 1 relationship?','Green','Yellow','Red','Blue','Yellow')
+#7('What color on the stop light method are 1 to many relationship?','Green','Yellow','Red','Blue','Green')
+#8('What color on the stop light method are many to many relationship?','Green','Yellow','Red','Blue','Red')
+#9('What entity is used to clear up many to many relationships?','Intersection Entities','Clear Entities','Janitor Entities','Secondary Entities','Intersection Entities')
+#10 ('How many entities are in a binary model?','1','2','3','4','2')
 
 #Table 3 labled Assably Prog
 curse.execute('''CREATE TABLE IF NOT EXISTS AssablyProg
               (question TEXT, answerOption1 TEXT, answerOption2 TEXT, answerOption3 TEXT, answerOption4 TEXT, correctAnswer TEXT)''')
 
+curse.execute('''INSERT INTO AssablyProg VALUES
+              (
+              ''')
+conn.commit()
 
-
+#1
 
 
 #Table 4 labeled PythonProg
